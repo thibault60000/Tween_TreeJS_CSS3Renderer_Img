d33728f6791cc5aa1840c70fe6cec535ab6bba77
clemv80 <clem.v80@gmail.com>
2018-04-16 21:13:32 +0200

    c bô

--- js/main3.js
+++ js/main3.js
@@ -1,9 +1,10 @@
-var camera, controls, scene, renderer, clock, sphere;
+var camera, controls, scene, renderer, clock;
 var particleSystem;
 var phi, theta;
 var objects = [];
 var targets = { table: [], sphere: [], helix: [], grid: [] };
 var projector, mouse = {x:0, y:0};
+var spheres = [];
 
 init();
 animate();
@@ -19,9 +20,6 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
-
-    sphere = new THREE.Group();
-    scene.add(sphere);
 
     projector = new THREE.Projector();
     document.addEventListener("click", function(event){
@@ -88,44 +86,24 @@ function init() {
 
     /* ******* SPHERE *********** */
 
-    buildSphere(pictures[0], 1000, 200, 200);
-    //buildSphere(pictures[1], 2500, 500, 500);
-    //buildSphere(pictures[2], 3000, 600, 600);
+    buildSphere(pictures[0], 1000, 0, 0, 4000, 200, 200);
+    buildSphere(pictures[1], 1200, 0, 0, 0, 200, 200);
+    buildSphere(pictures[2], 600, 0, 0, -4000, 200, 200);
 
-    /*var geometry = new THREE.SphereGeometry( 990, 32, 32 );
-    var material = new THREE.MeshBasicMaterial( {color: 0x000088} );
-    var ocean = new THREE.Mesh( geometry, material );
-    scene.add( ocean );*/
 
-    var particles = new THREE.Geometry;
-    for (var p = 0; p<=20000; p++) {
-        var particle = new THREE.Vector3(Math.random() * 10000-5000, Math.random() * 10000-5000, Math.random() * 10000-5000);
-        particles.vertices.push(particle);
-    }
-    // create the particle variables
-    var particleMaterial = new THREE.ParticleBasicMaterial({
-        color: 0xFFFFFF,
-        size: 20,
-        map: THREE.ImageUtils.loadTexture(
-            "./img/particle.png"
-        ),
-        blending: THREE.AdditiveBlending,
-        transparent: true
-    });
 
-// also update the particle system to
-// sort the particles which enables
-// the behaviour we want
-
-    particleSystem = new THREE.ParticleSystem(particles, particleMaterial);
-    scene.add(particleSystem);
+    buildParticlesSystem(3000, 20, 8000, 10000);
 
     window.addEventListener('resize', onWindowResize, false );
 }
 
-function buildSphere(pics, diameter, width, height){
-    var geometry = new THREE.BoxGeometry( 200, 200, 5 );
-    var vector = new THREE.Vector3();
+function buildSphere(pics, diameter, px, py, pz, width, height){
+    var geometry = new THREE.BoxGeometry( width, height, 5 );
+    var vector = new THREE.Vector3(px, py, pz);
+    var sphere = new THREE.Group();
+    sphere.position.x = px;
+    sphere.position.y = py;
+    sphere.position.z = pz;
 
     for ( var i = 0, l = pics.length; i < l; i ++ ) {
         phi = Math.acos( -1 + ( 2 * i ) / l );
@@ -138,15 +116,6 @@ function buildSphere(pics, diameter, width, height){
         var object = new THREE.Mesh( geometry, cubeMaterial );
 
 
-        /*var img = new THREE.MeshLambertMaterial({ //CHANGED to MeshBasicMaterial
-            map:THREE.ImageUtils.loadTexture(pics[i])
-        });
-        img.map.needsUpdate = true; //ADDED
-
-        // plane
-        var object = new THREE.Mesh(new THREE.PlaneGeometry(width, height),img);
-        object.overdraw = true;*/
-
         object.position.x = diameter * Math.cos( theta ) * Math.sin( phi );
         object.position.y = diameter * Math.sin( theta ) * Math.sin( phi );
         object.position.z = diameter * Math.cos( phi );
@@ -158,6 +127,38 @@ function buildSphere(pics, diameter, width, height){
         objects.push(object);
         sphere.add(object);
     }
+
+    spheres.push(sphere);
+    scene.add(sphere);
+}
+
+function buildParticlesSystem(quantity, size, minDistance, maxDistance){
+    var particles = new THREE.Geometry;
+    var spherical = new THREE.Spherical();
+
+    for (var p = 0; p<=quantity; p++) {
+        spherical.radius = Math.random() * (maxDistance-minDistance) + minDistance;
+        spherical.phi = Math.random() * Math.PI;
+        spherical.theta = Math.random() * Math.PI * 2;
+
+        var particle = new THREE.Vector3().setFromSpherical(spherical);
+
+        particles.vertices.push(particle);
+    }
+
+    var particleMaterial = new THREE.ParticleBasicMaterial({
+        color: 0xFFFFFF,
+        size: size,
+        map: THREE.ImageUtils.loadTexture(
+            "./img/particle3.png"
+        ),
+        blending: THREE.AdditiveBlending,
+        transparent: true
+    });
+
+    particleSystem = new THREE.ParticleSystem(particles, particleMaterial);
+    scene.add(particleSystem);
+
 }
 
 function onWindowResize() {
@@ -168,8 +169,12 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame( animate );
     var delta = clock.getDelta(); // Rotation Horloge
-    sphere.rotation.y += delta/20; // Rotation des particules
-    particleSystem.rotation.y -= delta/100; // Rotation des particules
+    spheres[1].rotation.y += delta/20;
+    for (var i=0; i<spheres.length; i++){
+        spheres[i].rotation.y += delta/20;
+    }
+
+    particleSystem.rotation.y -= delta/50; // Rotation des particules
     controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
     render();
 }
