e0caff02957474173b31822eed91c04bf4355926
clemv80 <clem.v80@gmail.com>
2018-04-16 14:26:31 +0200

    intersects

--- js/main3.js
+++ js/main3.js
@@ -1,12 +1,16 @@
-var camera, controls, scene, renderer;
+var camera, controls, scene, renderer, clock, sphere;
+var particleSystem;
 var phi, theta;
 var objects = [];
 var targets = { table: [], sphere: [], helix: [], grid: [] };
+var projector, mouse = {x:0, y:0};
 
 init();
 animate();
 
 function init() {
+    clock = new THREE.Clock;
+
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0x000000 );
     //scene.fog = new THREE.Fog(0xffffff, 0.0015, 10000);
@@ -16,6 +20,47 @@ function init() {
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
 
+    sphere = new THREE.Group();
+    scene.add(sphere);
+
+    projector = new THREE.Projector();
+    document.addEventListener("click", function(event){
+        // the following line would stop any other event handler from firing
+        // (such as the mouse's TrackballControls)
+        // event.preventDefault();
+
+        //console.log("Click.");
+
+        // update the mouse variable
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+        // find intersections
+
+        // create a Ray with origin at the mouse position
+        //   and direction into the scene (camera direction)
+        var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+        projector.unprojectVector( vector, camera );
+        var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+
+        // create an array containing all objects in the scene with which the ray intersects
+        var intersects = ray.intersectObjects( objects );
+
+        // if there is one (or more) intersections
+        if ( intersects.length > 0 )
+        {
+            var hit = intersects[0].object;
+            //console.log(hit);
+            console.log(camera);
+            //camera.position.set(hit.position.x,hit.position.y,hit.position.z);
+
+            //camera.target.position.copy( hit.position );
+            //camera.updateProjectionMatrix();
+            /*var lookAt = new THREE.Vector3();
+            lookAt.copy( hit.position ).multiplyScalar( 2 );
+            camera.lookAt( lookAt );*/
+        }
+    }, true);
 
     var container = document.getElementById( 'container' );
     container.appendChild( renderer.domElement );
@@ -42,10 +87,19 @@ function init() {
     //buildSphere(pictures[1], 2500, 500, 500);
     //buildSphere(pictures[2], 3000, 600, 600);
 
-    /*var geometry = new THREE.SphereGeometry( 4800, 32, 32 );
-    var material = new THREE.MeshBasicMaterial( {color: 0x404040} );
-    var sphere = new THREE.Mesh( geometry, material );
-    scene.add( sphere );*/
+    /*var geometry = new THREE.SphereGeometry( 990, 32, 32 );
+    var material = new THREE.MeshBasicMaterial( {color: 0x000088} );
+    var ocean = new THREE.Mesh( geometry, material );
+    scene.add( ocean );*/
+
+    var particles = new THREE.Geometry;
+    for (var p = 0; p<=20000; p++) {
+        var particle = new THREE.Vector3(Math.random() * 10000-5000, Math.random() * 10000-5000, Math.random() * 10000-5000);
+        particles.vertices.push(particle);
+    }
+    var particleMaterial = new THREE.ParticleBasicMaterial({ color: 0xeeeeee, size: 7 });
+    particleSystem = new THREE.ParticleSystem(particles, particleMaterial);
+    scene.add(particleSystem);
 
     window.addEventListener('resize', onWindowResize, false );
 }
@@ -78,13 +132,12 @@ function buildSphere(pics, diameter, width, height){
         object.position.y = diameter * Math.sin( theta ) * Math.sin( phi );
         object.position.z = diameter * Math.cos( phi );
 
-
         vector.copy( object.position ).multiplyScalar( 2 );
         object.lookAt( vector );
         targets.sphere.push( object );
 
         objects.push(object);
-        scene.add(object);
+        sphere.add(object);
     }
 }
 
@@ -95,6 +148,9 @@ function onWindowResize() {
 }
 function animate() {
     requestAnimationFrame( animate );
+    var delta = clock.getDelta(); // Rotation Horloge
+    sphere.rotation.y += delta/20; // Rotation des particules
+    particleSystem.rotation.y -= delta/100; // Rotation des particules
     controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
     render();
 }
